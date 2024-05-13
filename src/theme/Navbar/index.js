81c8863f709df8d027007820cede843d1a74031c
khaoula-ben-chaabane <khaoula.ben-chaabane@telekom.de>
2024-05-13 12:07:13 +0200

    improved header layout and added llm playground link button

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -3,6 +3,7 @@ import '@telekom/scale-components/dist/scale-components/scale-components.css';
 import '@telekom/scale-components-react';
 
 export default function Navbar() {
+
   return (
   <nav>
 <scale-telekom-header 
@@ -27,6 +28,23 @@ main-nav-aria-label="Main navigation">
             </scale-badge>
         </a>
         </scale-telekom-nav-item>
+
+        <scale-telekom-nav-item>
+        <a href="https://playground.llmhub.t-systems.net/">
+            <scale-badge no-dot label="LLM Playground">
+            <scale-icon-action-launch accessibility-title="launch"/>
+            </scale-badge>
+        </a>
+        </scale-telekom-nav-item>
+
+       {/* <scale-telekom-nav-item>
+        <button id="mode-toggle" class="click">
+        <scale-badge no-dot label="Mode">
+            <scale-icon-action-light-dark-mode accessibility-title="light-dark-mode"/>
+        </scale-badge>
+        </button>
+  </scale-telekom-nav-item>*/}
+        
 
       </scale-telekom-nav-list> 
     </scale-telekom-header>
