0053682d994061316fe4b2dd1b1dba43307d9e4e
Manvendra Singh Rathore <manvendra.rathore@t-systems.com>
2025-11-24 09:59:19 +0530

    feat: AIFS-14998 updating Documentation

--- src/components/PlansPageFeatures/PlansData/PlansModelData/index.js
+++ src/components/PlansPageFeatures/PlansData/PlansModelData/index.js
@@ -4,19 +4,13 @@ export const PlansHistory = AllPlansV1
 
 
 /**
- * Instructions to Update or Add New Plans on the Plans Page:
+ * Instructions to Update or Add Models and Pricing on the Plans Page:
  *
- * 1. Create a new folder named by the version (e.g., "v1.11") inside the PlansModelData directory.
- * 2. Add your JSON files for the Plans and Legal document for the latest version into this folder.
- * 3. Create an 'index.js' file in the new version folder to import and export the JSON files.
- *    - Refer to the 'index.js' in the 'v1.13' folder for structure and examples.
- * 4. Once all JSON files are imported and the version is exported from the new folder,
- *    import this version here in this file.
- * 5. Add a new object to the 'PlansHistory' array, including:
- *    - The exported version,
- *    - 'validFrom' date,
- *    - 'documentLink'.
- * 6. Update the 'Current' key in the PlansHistory objects to point to the version currently in use.
- *
+ * 1. To Maintain the history of plans configuration, please create a new folder with version number in the same folder (PlansModelData).
+ * 2. Go to AIFS Serving UI and export the Rate plans from Plans page. The Application will download a JSON file with all the plans and there model with pricing.
+ * 3. Add that file in your newly created folder.
+ * 4. Import that file in index.js in same folder to make it accessible in application.
+ * 5. Now update this file to access newly created file export variable.
+
  * Please follow these steps carefully to ensure the Plans are updated correctly.
  */
