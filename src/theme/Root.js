f86edc1eb6dc0d971723b228b279fd01c15cc2e1
Docs Update <docs@aifoundation.local>
2026-03-17 10:51:59 +0100

    Revert "feat: add Ask AI assistant with RAG-powered doc search"
    This reverts commit 85b8302a002a98669ae1b59c4857290550141267.

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import '../i18n'; // initialize i18next
-import AskAI from '../components/AskAI';
 
 export default function Root({ children }) {
-    return (
-        <>
-            {children}
-            <AskAI />
-        </>
-    );
+    return children;
 }
 
